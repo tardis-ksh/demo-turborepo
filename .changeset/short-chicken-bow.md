071a5df90de9e24e4cd8c2f399aff8e7e0ce83b7
tardis <14971857@qq.com>
2024-05-22 23:12:55 +0800

    feat: use acme hooks

---
+++ .changeset/short-chicken-bow.md
@@ -0,0 +1,6 @@
+---
+'@tardis-ksh/acme-hooks': minor
+'@tardis-ksh/core': minor
+---
+
+use hooks
--- packages/acme-core/src/components/Link/index.tsx
+++ packages/acme-core/src/components/Link/index.tsx
@@ -1,11 +1,15 @@
 import type { FC, PropsWithChildren } from 'react';
 
+import { useConsole } from "@tardis-ksh/acme-hooks";
+
 export interface LinkProps {
   href?: string;
 }
 
 const Link:FC<PropsWithChildren & LinkProps> = (props) => {
   const { children, href } = props;
+
+  useConsole();
 
   return <a href={href}>change {children}</a>;
 };
